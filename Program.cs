6dd46fc219a21e8e15f0b46056c8e24f71b050e8
Egorova Tatiana <tanyasha.89@mail.ru>
2022-08-28 21:25:42 +0300

    seminars7homework task52

--- Program.cs
+++ Program.cs
@@ -33,7 +33,7 @@ Console.Write("Input number of columns: ");
 int n = Convert.ToInt32(Console.ReadLine());
 */
 
-
+/*
 //Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
 
 Console.Write("Input number of rows: ");
@@ -69,12 +69,62 @@ void Show2dArray(int[,] array)
 {
     for (int i = 0; i < array.GetLength(0); i++)
     {
-        Console.Write("[ ");
         for (int j = 0; j < array.GetLength(1); j++)
         {
             Console.Write(array[i,j] + " ");
         }   
-        Console.Write("]");
         Console.WriteLine(""); 
     }
 }
+*/
+/*
+//Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+Console.Write("Input number of rows: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input number of columns: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int[,] numbers = new int[n, m];
+CreatRandom2dArray(numbers);
+
+
+for (int j = 0; j < numbers.GetLength(1); j++)
+{
+    double avarage = 0;
+    for (int i = 0; i < numbers.GetLength(0); i++)
+    {
+        avarage = (avarage + numbers[i, j]);
+    }
+    avarage = avarage / n;
+    Console.Write(avarage + "; ");
+}
+Console.WriteLine();
+ShowArray(numbers);
+
+
+
+void CreatRandom2dArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(0, 10);
+        }
+    }
+}
+
+void ShowArray(int[,] array)
+{
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine("");
+    }
+}
+*/
